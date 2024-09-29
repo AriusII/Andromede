f7163ecf031de122e5d88f5b0f412435bf3af8c0
AriusII <ariusdev@outlook.com>
2024-09-29 04:27:53 +0200

    Init

---
+++ Srcs/Andromede.Srcs.Internals.Users/Endpoints/UsersEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using UsersService;
+using UsersServiceBase = UsersService.UsersService.UsersServiceBase;
+
+namespace Andromede.Srcs.Internals.Users.Endpoints;
+
+internal sealed class UsersEndpoint : UsersServiceBase
+{
+    public override Task<GetUserByIdResponse> GetUserById(GetUserByIdRequest request, ServerCallContext context)
+    {
+        return base.GetUserById(request, context);
+    }
+
+    public override Task<CreateUserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
+    {
+        return base.CreateUser(request, context);
+    }
+
+    public override Task<UpdateUserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+    {
+        return base.UpdateUser(request, context);
+    }
+
+    public override Task<DeleteUserByIdResponse> DeleteUserById(DeleteUserByIdRequest request, ServerCallContext context)
+    {
+        return base.DeleteUserById(request, context);
+    }
+}
