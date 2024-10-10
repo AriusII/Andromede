d9399c9762c249e3771743eae9320d3175e47fc8
jcoureuil <jcoureuil@adoria.com>
2024-10-10 14:00:04 +0200

    .

--- Orchestrator/Andromede.Orchestrator.Aspire/Program.cs
+++ Orchestrator/Andromede.Orchestrator.Aspire/Program.cs
@@ -13,21 +13,14 @@ var redis = builder
     .WithImage("redis")
     .WithDataVolume("redis-data");
 
-var serviceLogging = builder
-    .AddProject<Andromede_Srcs_Services_Logging>("Internal-Service-Logging")
-    .WithReference(sqlServer);
-
-var serviceMailling = builder
-    .AddProject<Andromede_Srcs_Services_Mailling>("Internal-Service-Mailling");
-
 var internalUsers = builder
     .AddProject<Andromede_Srcs_Internals_Users>("Internal-Api-Users")
     .WithReference(redis)
     .WithReference(sqlServer)
     .WithReplicas(2);
 
-var externalAuthentication = builder
-    .AddProject<Andromede_Srcs_Externals_Authentication>("External-Bff-Authentication")
+var gatewayAuthentication = builder
+    .AddProject<Andromede_Srcs_Gateway_Authentication>("Gateway-Authentication")
     .WithReference(redis)
     .WithReference(internalUsers);
 
